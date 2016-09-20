cecd122e9d2c51ecf84ab26ff4213e724f784970
Pedro <gomes.pedro15@gmail.com>
2016-09-20 20:03:03 -0300

    Adicionada as funções para escrever a árvore e o texto no arquivo.

--- Huffman/Compactador v1.3/compactar.c
+++ Huffman/Compactador v1.3/compactar.c
@@ -1,29 +1,67 @@
 #include "f_arvore.h"
 
-/* Função para escrever as informações no arquivo *.huff */
-void escrever_arquivo(Node *cabeca, unsigned short lixo, unsigned short tamanho_arvore)
+/* FUNÇÃO PARA ESCREVER AS INFORMAÇÕES NO ARQUIVO *.huff */
+void escrever_arquivo(Node *cabeca, unsigned short lixo, unsigned short tamanho_arvore, char *array_texto, int tamanho)
 {
-    /* Cria novo arquivo.huff, caso já exista, o arquivo não é criado */
+    /* CRIA NOVO ARQUIVO *.HUFF, CASO JÁ EXISTA NÃO É CRIADO */
     FILE *novo_arquivo = fopen("arquivo.huff", "w+x");
 
-    /* Como o tamanho total do lixo mais o tamanho da árvore é de 16 bits, realizei a soma
-       do lixo deslocado 13 vezes para a esquerda e do tamanho. */
-    unsigned short cabecalho_inicial = converter_lixo(lixo) + tamanho_arvore;
+    /* COMO O TAMANHO TOTAL DO LIXO MAIS O TAMANHO DA ÁRVORE É DE 16 BITS, REALIZEI A SOMA
+       DO LIXO DESLOCADO 13 VEZES PARA A ESQUERDA E DO TAMANHO DA ÁRVORE. */
+    unsigned short cabecalho_inicial[1] = {converter_lixo(lixo) + tamanho_arvore;}
 
-    /* Escreve os dados no arquivo na ordem: lixo (3 bits), tamanho da árvore(13 bits),
-       arvore e texto(compactado) */
-    fputs(cabecalho_inicial, novo_arquivo);
-    /// fputs(print_pre_ordem_arvore(cabeca), novo_arquivo);
-    /// fputs(escrever_texto, novo_arquivo);
+    /* ESCREVE OS DADOS NO ARQUIVO NA ORDEM: LIXO (3 BITS), TAMANHO DA ÁVORE(13 BITS),
+       ÁRVORE E TEXTO COMPACTADO */
+    fwrite(cabecalho_inicial, sizeof(short), sizeof(cabecalho_inicial), novo_arquivo);
+
+    /* ESCREVE A ÁRVORE EM PRÉ-ORDEM */
+    escrever_árvore(cabeca, novo_arquivo);
+    /* ESCREVE O TEXTO JÁ COMPACTADO */
+    escrever_texto(array_texto, tamanho, novo_arquivo);
+
+    /* FECHA O ARQUIVO */
+    fclose(novo_arquivo);
 }
 
-/* Desloca o lixo 13 vezes para a esquerda */
+/* DESLOCA O LIXO 13 VEZES PARA A ESQUERDA */
 unsigned short converter_lixo(unsigned short lixo)
 {
     return lixo << 13;
 }
 
-void escrever_texto(Node *cabeca)
+/* ESCREVE A ÁRVORE NO ARQUIVO */
+void escrever_árvore(Node *cabeca, FILE *arquivo)
 {
+    if(cabeca != NULL)
+    {
+        fputc(cabeca->letra, arquivo);
+        print_pre_ordem_arvore(cabeca->filho_esquerda);
+        print_pre_ordem_arvore(cabeca->filho_direita);
+    }
+}
 
+void escrever_texto(char *array_texto, int tamanho, File *arquivo)
+{
+    int i;
+    unsigned char c = 0;
+
+    for(i = 0; i < size; i++)
+    {
+        if(array_texto[i] == 1)
+        {
+            c = set_bit(c, (i % 8 + 1));
+        }
+
+        if(i % 8 == 1)
+        {
+            fputs(c, arquivo);
+            c = 0;
+        }
+    }
+}
+
+char set_bit(unsigned char c, int i)
+{
+    unsigned char mask = 1 << i;
+    return mask | c;
 }
--- Huffman/Compactador v1.3/compactar.h
+++ Huffman/Compactador v1.3/compactar.h
@@ -2,4 +2,8 @@ void escrever_arquivo(Node *cabeca, unsigned short lixo, unsigned short tamanho_
 
 unsigned short converter_lixo(unsigned short lixo);
 
-void escrever_texto(Node *cabeca);
+void escrever_árvore(Node *cabeca, FILE *arquivo);
+
+void escrever_texto(int *array_texto, File *arquivo);
+
+char set_bit(unsigned char c, int i);
