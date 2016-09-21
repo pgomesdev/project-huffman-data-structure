6e1b48292cc38f8ff3614a09a9289a73e60b9dd3
Pedro <gomes.pedro15@gmail.com>
2016-09-21 19:11:24 -0300

    Retirado o número mágico "8" e feito mais algumas correções.

--- Huffman/Compactador v1.3/compactar.c
+++ Huffman/Compactador v1.3/compactar.c
@@ -1,28 +1,31 @@
 #include "f_arvore.h"
+#define TAMANHO_BYTE 8
 
+/* FUNÇÃO UTILIZADA PARA SETAR UM DETERMINADO BIT EM UM CHAR */
 unsigned char set_bit(unsigned char c, int i)
 {
     unsigned char mask = 1 << i;
     return mask | c;
 }
 
-/* DESLOCA O LIXO 13 VEZES PARA A ESQUERDA */
+/* FAZ O DESLOCAMENTO DOS 3 BITS DE LIXO PARA OS TRÊS PRIMEIROS BITS DO SHORT */
 unsigned short converter_lixo(unsigned short lixo)
 {
     return lixo << 13;
 }
 
-/* ESCREVE A ÁRVORE NO ARQUIVO */
-void escrever_árvore(Node *cabeca, FILE *arquivo)
+/* ESCREVE A ÁRVORE EM PRÉ-ORDEM NO ARQUIVO */
+void escrever_arvore(Node *cabeca, FILE *arquivo)
 {
     if(cabeca != NULL)
     {
         fputc(cabeca->letra, arquivo);
-        print_pre_ordem_arvore(cabeca->filho_esquerda);
-        print_pre_ordem_arvore(cabeca->filho_direita);
+        escrever_arvore(cabeca->filho_esquerda);
+        escrever_arvore(cabeca->filho_direita);
     }
 }
 
+/* ESCREVE O TEXTO JÁ COMPACTADO NO ARQUIVO */
 void escrever_texto(char *array_texto, int tamanho, File *arquivo)
 {
     int i;
@@ -32,10 +35,10 @@ void escrever_texto(char *array_texto, int tamanho, File *arquivo)
     {
         if(array_texto[i] == 1)
         {
-            c = set_bit(c, (i % 8 + 1));
+            c = set_bit(c, (i % TAMANHO_BYTE + 1));
         }
 
-        if(i % 8 == 1)
+        if(i % TAMANHO_BYTE == 1)
         {
             fputs(c, arquivo);
             c = 0;
--- Huffman/Compactador v1.3/compactar.h
+++ Huffman/Compactador v1.3/compactar.h
@@ -1,7 +1,13 @@
+/* FUNÇÃO UTILIZADA PARA SETAR UM DETERMINADO BIT EM UM CHAR
+   RETORNA UM CARACTER JÁ COM O BIT SETADO */
 unsigned char set_bit(unsigned char c, int i);
 
+/* FAZ O DESLOCAMENTO DOS 3 BITS DE LIXO PARA OS TRÊS PRIMEIROS BITS DO SHORT
+   RETORNA OS 3 BITS DE LIXO POSICIONADO NOS TRÊS PRIMEIROS BITS */
 unsigned short converter_lixo(unsigned short lixo);
 
-void escrever_árvore(Node *cabeca, FILE *arquivo);
+/* ESCREVE A ÁRVORE EM PRÉ-ORDEM NO ARQUIVO */
+void escrever_arvore(Node *cabeca, FILE *arquivo);
 
+/* ESCREVE O TEXTO JÁ COMPACTADO NO ARQUIVO */
 void escrever_texto(unsigned char *array_texto, int tamanho, FILE *arquivo);
