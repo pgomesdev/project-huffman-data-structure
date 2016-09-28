930ca127a663ba61c80c6bdf617a989588fdd4f7
BlackPauloBR <pauloroberto_93@live.com>
2016-09-28 15:37:43 -0300

    retirada funções que eram usada na primeira implementação de huffman
    nesse versão essas funções são inuteis por isso foram removidas

--- Huffman/f_arquivo.h
+++ Huffman/f_arquivo.h
@@ -2,15 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void print_freq_c(char *freq, int tam);
-
-void print_freq_i(int *freq, int tam);
-
-void print_txt(char *txt, int tam);
+void print_txt(unsigned char *txt, int tam);
 
 int contar_tam_texto_char(FILE *arq);
 
 FILE *abrir_Arquivo(char *url);
 
-void limpar_array_i(int *arq, int tam);
-void limpar_array_c(char *arq, int tam);
