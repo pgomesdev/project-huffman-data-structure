651a5455b4c166d81f469df5f0eae4d2a5b1ff04
Tarciso-Filho <tarciso.filho@hotmail.com>
2016-09-29 18:48:32 -0300

    Inicio da Comentação do arquivo f_arvore.h
    Explicação das bibliotecas chamadas e parcial da estrutura Nó.

--- Huffman/f_arvore.h
+++ Huffman/f_arvore.h
@@ -1,14 +1,26 @@
+/// INSTRUÇÃO PARA O COMPILADOR SÓ IMPORTAR AS BIBLIOTECAS ABAIXO QUE NÃO EXISTIREM
 #pragma once
-#include <stdlib.h>
-#include <stdio.h>
-#include <locale.h>
-typedef struct Node Node;
 
+#include <stdlib.h>// Biblioteca para abrir arquivos
+#include <stdio.h>// Biblioteca padrão de Entrada e Saida do C
+
+/// BIBLIOTECA DE REGIONALIZAÇÃO DO C (UTILIZADA NO ARQUIVO MAIN)
+#include <locale.h>
+
+typedef struct Node Node;// Para se referir a estrutura nó usar:Node
+
+/// DEFINIÇÃO DE NÓ DE ACORDO COM OS NÓS DEFINIDOS POR HUFFMAN NA ARVORE DE HUFFMAN
 struct Node
 {
+    /// FREQUÊNCIA DA variavél LETRA NO TEXTO
     int num;
+    
+    /// CARACTERE CONTIDO NO TEXTO
     unsigned char letra;
+    
+    ///variavél QUE CONTÉM A PROFUNDIDADE DESSE NÓ NA ARVORE DE HUFFMAN
     int profundidade;
+    
     Node *proximo_node;
     Node *filho_esquerda;
     Node *filho_direita;
