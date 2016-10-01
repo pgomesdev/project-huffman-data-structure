cc2e388a676a8ed7ee0d270b573c96f40cdeeb46
BlackPauloBR <pauloroberto_93@live.com>
2016-09-30 22:17:27 -0300

    apenas apaguei uma função que nao ultilizava mais
    //FUNÇÃO DESCONTINUADA
    void lista_de_folhas(Node *cabeca_lista);

--- Huffman/f_lista.h
+++ Huffman/f_lista.h
@@ -1,42 +1,38 @@
-/// INSTRU√á√ÉO PARA O COMPILADOR S√ì IMPORTAR AS BIBLIOTECAS ABAIXO QUE N√ÉO EXISTIREM
+/// INSTRU«√O PARA O COMPILADOR S” IMPORTAR AS BIBLIOTECAS ABAIXO QUE N√O EXISTIREM
 #pragma once
+#include "f_arvore.h"// Arquivo que possui as definicıes iniciais da ¡rvore de Huffman
 
-#include "f_arvore.h"// Arquivo que possui as definic√µes iniciais da √Årvore de Huffman
+/// NESSA BIBLIOTECA EST√O AS FUN«’ES PARA A LISTA DE HUFFMAN
 
-/// NESSA BIBLIOTECA EST√ÉO AS FUN√á√ïES PARA A LISTA DE HUFFMAN
-
-//Fun√ß√£o que n√£o recebe vari√°veis;
-//e devolve um ponteiro para N√≥s, inicializado com NULL
+//FunÁ„o que n„o recebe vari·veis;
+//e devolve um ponteiro para NÛs, inicializado com NULL
 Node *criar_Node_NULL();
 
-//Fun√ß√£o que recebe: a lista de n√≥s de Huffman, como um ponteiro para n√≥s, apontada por cabeca_lista
+//FunÁ„o que recebe: a lista de nÛs de Huffman, como um ponteiro para nÛs, apontada por cabeca_lista
 //e devolve 0, se a lista passada conter algum elemento,
 //ou devolve um inteiro, se a lista estiver vazia.
 int esta_Vazia(Node *cabeca_lista);
 
-//Fun√ß√£o que recebe: a lista de n√≥s de Huffman, como um ponteiro para n√≥s, apontada por cabeca_lista;
-//um caractere a ser inserido na lista, armazenado como unsigned char na vari√°vel letra;
-//a frequ√™ncia desse caractere no texto, armazenado no inteiro num;
-//e devolve um ponteiro para N√≥s como o primeiro n√≥ da nova lista, contendo o novo elemento.
+//FunÁ„o que recebe: a lista de nÛs de Huffman, como um ponteiro para nÛs, apontada por cabeca_lista;
+//um caractere a ser inserido na lista, armazenado como unsigned char na vari·vel letra;
+//a frequÍncia desse caractere no texto, armazenado no inteiro num;
+//e devolve um ponteiro para NÛs como o primeiro nÛ da nova lista, contendo o novo elemento.
 Node *add_Node_meio_ordenado(Node *cabeca_lista, unsigned char letra, int num);
 
-//Fun√ß√£o que recebe a lista de n√≥s de Huffman, como um ponteiro para n√≥s, apontada por cabeca_lista
-//e que n√£o possui nenhuma devolu√ß√£o. FUN√á√ÉO RECURSIVA.
-//Sua a√ß√£o √© utilizar o printf para imprimir na tela o conteudo da Lista de Huffman. Se estiver vazia, n√£o excuta a√ß√£o.
+//FunÁ„o que recebe a lista de nÛs de Huffman, como um ponteiro para nÛs, apontada por cabeca_lista
+//e que n„o possui nenhuma devoluÁ„o. FUN«√O RECURSIVA.
+//Sua aÁ„o È utilizar o printf para imprimir na tela o conteudo da Lista de Huffman. Se estiver vazia, n„o excuta aÁ„o.
 // Modelo:   [%c]
 void print_lista_Frequencia(Node *cabeca_lista);
 
-//Fun√ß√£o que recebe: a lista de n√≥s de Huffman (preferencialmente vazia), como um ponteiro, apontada por cabeca_lista;
-//o texto a ser analizado, como uma String, que ser√° apontada por txt;
+//FunÁ„o que recebe: a lista de nÛs de Huffman (preferencialmente vazia), como um ponteiro, apontada por cabeca_lista;
+//o texto a ser analizado, como uma String, que ser· apontada por txt;
 //o tamanho do texto recebido, armazenado no inteiro tam, onde tam >= 0;
-//e devolve a lista de n√≥s de Huffman, com elementos ordenados pela frequ√™ncia do caractere, de acordo com o texto passado.
+//e devolve a lista de nÛs de Huffman, com elementos ordenados pela frequÍncia do caractere, de acordo com o texto passado.
 Node *criar_lista_Frequencia(Node *cabeca_lista, unsigned char *txt, int tam);
 
-//Fun√ß√£o que recebe a lista de n√≥s de Huffman, como um ponteiro para n√≥s, apontada por cabeca_lista
+//FunÁ„o que recebe a lista de nÛs de Huffman, como um ponteiro para nÛs, apontada por cabeca_lista
 //e devolve a quantidade de elementos na lista no formato inteiro.
-//FUN√á√ÉO N√ÉO UTILIZADA
+//FUN«√O N√O UTILIZADA
 int calcular_tam_lista(Node *cabeca_lista);
-
-//FUN√á√ÉO DESCONTINUADA
-void lista_de_folhas(Node *cabeca_lista);
 
