7fd3e7aea40bd0799e669cc5c0d9294fd0a193df
BlackPauloBR <pauloroberto_93@live.com>
2016-09-30 22:16:24 -0300

    apenas coloquei algumas chamadas no final do main pra verificar se as funções hash estão funcionando

--- Huffman/main.c
+++ Huffman/main.c
@@ -1,94 +1,123 @@
-#include "f_arquivo.h"// Fun√ß√µes para manipular o arquivo .txt ou seu conte√∫do
-#include "f_arvore.h"// TAD da √Årvore de Huffman como Arvore
+#include <locale.h>// BIBLIOTECA DE REGIONALIZA«√O DO C (UTILIZADA NO ARQUIVO MAIN)
+#include "f_arquivo.h"// FunÁıes para manipular o arquivo .txt ou seu conte˙do
+#include "f_arvore.h"// TAD da ¡rvore de Huffman como Arvore
 #include "compactar.h"
 #include "descompactar.h"
-#include "f_lista.h"// Fun√ß√µes para utilizar a √Årvore de Huffman na estrutura de Lista
+#include "f_lista.h"// FunÁıes para utilizar a ¡rvore de Huffman na estrutura de Lista
+#include "f_hashtable.h"
 
-//N√£o recebe parametros, devolve 0 se chegar ao fim do programa e aqui come√ßa a execu√ß√£o do (Des)Compactador
+//N„o recebe parametros, devolve 0 se chegar ao fim do programa e aqui comeÁa a execuÁ„o do (Des)Compactador
 int main()
 {
-    /// FUN√á√ÉO QUE DETERMINA A REGIONALIZA√á√ÉO DO C√ìDIGO PARA PADR√ÉO. ESSENCIAL PARA IMPRIMIR NA TELA CARACTERES ESPECIAIS
+    /// FUN«√O QUE DETERMINA A REGIONALIZA«√O DO C”DIGO PARA PADR√O. ESSENCIAL PARA IMPRIMIR NA TELA CARACTERES ESPECIAIS
     setlocale(LC_ALL,"");
 
-    // String url salva a URL do arquivo texto que ser√° lido
-    char url[]="arquivos_txt_para_testes\\exemplo.txt";
+    // String url salva a URL do arquivo texto que ser· lido
+    char url[]="arquivos_txt_para_testes\\eulaENU.txt";
 
-    /// A FUN√á√ÉO "abrir_Arquivo()" RETORNA UM PONTEIRO DO TIPO "FILE", QUE APONTA PARA ARQUIVO.
+    /// A FUN«√O "abrir_Arquivo()" RETORNA UM PONTEIRO DO TIPO "FILE", QUE APONTA PARA ARQUIVO.
     FILE *arq = abrir_Arquivo(url);
-    
-    /// -x- A PARTIR DESSE PONTO, J√Å TEMOS ACESSO AO ARQUIVO .txt -x-
+
+
+
+    /// -x- A PARTIR DESSE PONTO, J¡ TEMOS ACESSO AO ARQUIVO .txt -x-
+
+
 
     ///  SE "abrir_Arquivo()" RETORNAR DIFERENTE DE ZERO, ARQUIVO ABERTO COM SUCESSO.
     if(arq != 0)
     {
         int tam = contar_tam_texto_char(arq);//Calculando o tamanho do texto
-        
-        /// LEMBRAR QUE UNSIGNED SERVE PARA UTILIZAR TODOS OS BITS DO CHAR E N√ÉO PERDER O BIT DE SINAL
-        unsigned char txt[tam];//Declarando o array que possui o mesmo tamanho do texto.
-        
-        char letra;//Vari√°vel condicional da repeti√ß√£o
-        int i = 0;//Vari√°vel de indice do array txt
 
-        /// OBS.: FOI NECESSARIO ULTILIZAR A FUN√á√ÉO "rewind()" == "FUN√á√ÉO QUE REAPONTA "fgetc()" PARA O INICIO DO ARQUIVO."
-        /// POIS NA CHAMADA DE "contar_tam_texto_char(arq)" , A FUN√á√ÉO "fgetc()" ESTAVA APONTANDO PARA O FINAL DO ARQUIVO.
+        /// LEMBRAR QUE UNSIGNED SERVE PARA UTILIZAR TODOS OS BITS DO CHAR E N√O PERDER O BIT DE SINAL
+        unsigned char txt[tam];//Declarando o array que possui o mesmo tamanho do texto.
+        char letra;//Vari·vel condicional da repetiÁ„o
+        int i = 0;//Vari·vel de indice do array txt
+
+        /// OBS.: FOI NECESSARIO ULTILIZAR A FUN«√O "rewind()" == "FUN«√O QUE REAPONTA "fgetc()" PARA O INICIO DO ARQUIVO."
+        /// POIS NA CHAMADA DE "contar_tam_texto_char(arq)" , A FUN«√O "fgetc()" ESTAVA APONTANDO PARA O FINAL DO ARQUIVO.
         rewind(arq);
-        
-        /// REPETI√á√ÉO FAZ COPIA DO CONTEUDO DE "arq" PARA BUFFER "txt[]".
+
+        /// REPETI«√O FAZ COPIA DO CONTEUDO DE "arq" PARA BUFFER "txt[]".
         while((letra = fgetc(arq)) != EOF)
         {
             txt[i] = letra;
             i++;
         }
-        
-        /// N√ÉO PRECISO MAIS DO ARQUIVO!
+        /// NAO PRECISO MAIS DO ARQUIVO!
         fclose(arq);
-        print_txt(txt,tam);//Verifica√ß√£o do Buffer, deve imprimir o mesmo texto de "url".
-        
-        /// -x- A PARTIR DESSE PONTO, J√Å TEMOS O TEXTO DO ARQUIVO .txt SALVO NO BUFFER -x-
+        //print_txt(txt,tam);//VerificaÁ„o do Buffer, deve imprimir o mesmo texto de "url".
 
-        // O ponteiro para N√≥s cabeca_lista √© declarado e inicializado com NULL
+
+
+        /// -x- A PARTIR DESSE PONTO, J¡ TEMOS O TEXTO DO ARQUIVO .txt SALVO NO BUFFER -x-
+
+
+
+        // O ponteiro para NÛs cabeca_lista È declarado e inicializado com NULL
         Node *cabeca_lista = criar_Node_NULL();
-        
-        /// RECEBEMOS UMA LISTA DE ELEMENTOS QUE CONTEM OS CARACTERES DISTINTOS E A FREQU√äNCIA DELES NO TEXTO
+
+        /// RECEBEMOS UMA LISTA DE ELEMENTOS QUE CONTEM OS CARACTERES DISTINTOS E A FREQU NCIA DELES NO TEXTO
         cabeca_lista = criar_lista_Frequencia(cabeca_lista,txt,tam);
-        
-        print_lista_Frequencia(cabeca_lista);//Verifica√ß√£o da Lista de Huffman, deve ter todos os caracteres distintos.
-        puts("\n");//Pula a linha
-        
-        /// -x- A PARTIR DESSE PONTO, J√Å TEMOS A LISTA DE HUFFMAN. -x-
+        print_lista_Frequencia(cabeca_lista);//VerificaÁ„o da Lista de Huffman, deve ter todos os caracteres distintos.
 
-        /// UTILIZAMOS UNSIGNED PARA N√ÉO PERDER O BIT DO SINAL.
-        /// J√Å O SHORT, POIS ELE UTILIZA 2 BYTES PARA ARMAZENAR VALORES, EXATAMENTE OS 2 BYTES INICIAIS DO CABE√áALHO
-        unsigned short lixo = 0;// Vari√°vel para armazenar quantos bits do √∫ltimo byte n√£o ser√£o significativos.
-        unsigned short tamanho_arvore = 0;// Vari√°vel para armazenar quantos n√≥s tem na arvore de Huffman.
+        puts("\n");
 
-        Node *cabeca_arvore = cabeca_lista;//Criamos uma v√°riavel que aponta para o inicio da Lista
-        
+
+
+        /// -x- A PARTIR DESSE PONTO, J¡ TEMOS A LISTA DE HUFFMAN. -x-
+
+
+
+        /// UTILIZAMOS UNSIGNED PARA N√O PERDER O BIT DO SINAL.
+        /// J¡ O SHORT, POIS ELE UTILIZA 2 BYTES PARA ARMAZENAR VALORES, EXATAMENTE OS 2 BYTES INICIAIS DO CABE«ALHO
+        unsigned short lixo = 0;// Vari·vel para armazenar quantos bits do ˙ltimo byte n„o ser„o significativos.
+        unsigned short tamanho_arvore = 0;// Vari·vel para armazenar quantos nÛs tem na arvore de Huffman.
+
+        Node *cabeca_arvore = cabeca_lista;//Criamos uma v·riavel que aponta para o inicio da Lista
+
         /// AGORA O PONTEIRO cabeca_arvore RECEBE A ARVORE DE HUFFMAN, GERADA A PARTIR DA LISTA ANTERIOR
         cabeca_arvore = criar_arvore_huffman(cabeca_arvore);
-        
-        /// CALCULA A PROFUNDIDADE DE CADA N√ì DA ARVORE E SALVA NA ESTRUTURA DO N√ì DE HUFFMAN
-        calcular_profundidade_nodes(cabeca_arvore,cabeca_arvore->profundidade);
-        
-        print_pre_ordem_arvore(cabeca_arvore);// Verifica√ß√£o da √Årvore de Huffman, deve estar todos os n√≥s em Pr√©-Ordem
 
-        /// A V√ÅRIAVEL lixo RECEBE QUANTOS BITS DO ULTIMO BYTE S√ÉO REPRESENTATIVOS (E 0 SE TODOS O S√ÉO)
+        /// CALCULA A PROFUNDIDADE DE CADA N” DA ARVORE E SALVA NA ESTRUTURA DO N” DE HUFFMAN
+        calcular_profundidade_nodes(cabeca_arvore,cabeca_arvore->profundidade);
+        print_pre_ordem_arvore(cabeca_arvore);// VerificaÁ„o da ¡rvore de Huffman, deve estar todos os nÛs em PrÈ-Ordem
+
+        /// A V¡RIAVEL lixo RECEBE QUANTOS BITS DO ULTIMO BYTE S√O REPRESENTATIVOS (E 0 SE TODOS O S√O)
         lixo = calcular_lixo(cabeca_arvore, lixo);
-        
-        /// FAZEMOS A OPERA√á√ÉO TERNARIA, DE MODO A SALVAR 0, SE N√ÉO POSSUI LIXO NO ULTIMO BYTE; (8-lixo) SE HOUVER LIXO
+
+        /// FAZEMOS A OPERA«√O TERNARIA, DE MODO A SALVAR 0, SE N√O POSSUI LIXO NO ULTIMO BYTE; (8-lixo) SE HOUVER LIXO
         lixo = lixo != 0 ? (8 - lixo): 0 ;
-        
-        /// A V√ÅRIAVEL tamanho_arvore RECEBE A QUANTIDADE DE N√ìS NA ARVORE DE HUFFMAN
+
+        /// A V¡RIAVEL tamanho_arvore RECEBE A QUANTIDADE DE N”S NA ARVORE DE HUFFMAN
         tamanho_arvore = calcular_tam_arvore(cabeca_arvore, tamanho_arvore);
 
-        print_lista_Frequencia(cabeca_lista);// Verifica√ß√£o da Lista de Huffman, deve provar que a Lista existe.
-        puts("\n");// Pula uma linha
-        
-        /// -x- A PARTIR DESSE PONTO, J√Å TEMOS A ARVORE DE HUFFMAN E AS INFORMA√á√ïES DO CABE√áALHO. -x-
-        
-        //lista_de_folhas(cabeca_lista);
 
-        //print_lista_Frequencia(cabeca_lista);
+
+        /// -x- A PARTIR DESSE PONTO, J¡ TEMOS A ARVORE DE HUFFMAN E AS INFORMA«’ES DO CABE«ALHO. -x-
+
+
+
+        puts("\n");
+        Hashtable *ht = create_hashtable();
+        Element *lista = criar_node_hash_null();
+
+        construir_ht(cabeca_arvore,lista,ht);
+
+
+        cabeca_arvore = remove_arvore(cabeca_arvore);//remove_arvore da um free() na arvore inteira.
+        print_ht(ht);// imprime a hash criada baseada na arvore
+
+        ///Se lista == NULL , ent„o a funcao construir_ht foi execultada com sucesso.
+        if(lista == NULL)
+        {
+            puts("construir_ht OK!!");
+        }
+        else
+        {
+            puts("construir_ht nao concluido\n");
+        }
+        ht = remove_hashtable(ht);//remove_hashtable da um free() em todas as listas da hash e na propia hash
 
         /*FILE *novo_arquivo = fopen("arquivo.huff", "w+x");
 
