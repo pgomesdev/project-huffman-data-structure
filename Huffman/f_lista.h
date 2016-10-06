53eb40db96ec8d532553af18edefda07652a6ba7
BlackPauloBR <pauloroberto_93@live.com>
2016-10-05 22:46:26 -0300

    atualização do .h para aceitar o progresso

--- Huffman/f_lista.h
+++ Huffman/f_lista.h
@@ -17,7 +17,8 @@ int esta_Vazia(Node *cabeca_lista);
 //um caractere a ser inserido na lista, armazenado como unsigned char na variável letra;
 //a frequência desse caractere no texto, armazenado no long long int num;
 //e devolve um ponteiro para Nós como o primeiro nó da nova lista, contendo o novo elemento.
-Node *add_Node_meio_ordenado(Node *cabeca_lista, unsigned char letra, unsigned long long int num);
+//o Nó progresso é apenas para print do loading, em progresso->num temos o progresso atual.
+Node *add_Node_meio_ordenado(Node *cabeca_lista, unsigned char letra, unsigned long long int num, Node *progresso);
 
 //Função que recebe a lista de nós de Huffman, como um ponteiro para nós, apontada por cabeca_lista
 //e que não possui nenhuma devolução. FUNÇÃO RECURSIVA.
