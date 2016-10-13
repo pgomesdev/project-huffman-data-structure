7433366e825e0e4a012dbdadf2fcc6f8538d00e0
BlackPauloBR <pauloroberto_93@live.com>
2016-10-13 15:55:37 -0300

    Add files via upload

---
+++ Cont.%20e%20Plot/f_lista.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include "f_arvore_binaria.h"
+#define MAX 1000
+
+typedef struct node
+{
+    int item;
+    struct node *proximo_node;
+} Node;
+
+Node *criar_lista_encadeada();
+
+int esta_vazio(Node *lista);
+
+Node *inserir_node(Node *lista, int item);
+
+void print_lista(Node *lista);
+
+int pesquisar_Node(Node *lista, int item);
+
+Node *removeNode(Node *lista, int item);
+
+Node *remove_lista(Node *lista);
+
+void print_array(int *numeros);
+
+void escrever_contagem(FILE *arquivo, ArvB *cabeca, Node *lista, int *numeros);
+
+void Quick_Sort(int *numeros, int inicio, int fim);
